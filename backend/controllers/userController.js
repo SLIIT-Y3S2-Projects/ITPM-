0e55f4cd3cd81c772e99eedca4558061839d7ec1
Dinula Fonseka <154993535+DCF2001@users.noreply.github.com>
2025-04-30 22:12:24 +0530

    Change the acees in controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -82,7 +82,7 @@ const getMe = asyncHandler(async (req, res) => {
 
 // @desc    Update user profile
 // @route   PUT /api/users/me
-// @access  Private
+// @access  private
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id);
 
